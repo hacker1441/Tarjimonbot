c9d597bbefdb64e6298805c28551cdec3c10c81a
Hacker <139839887+hacker1441@users.noreply.github.com>
2023-07-25 09:31:57 +0500

    Tarjimater
    Tarjima qiberadi

---
+++ utils.py
@@ -0,0 +1,9 @@
+from googletrans import Translator
+
+translator = Translator()
+
+async def translate_user_text(text, lang):
+    result = translator.translate(text=text, dest=lang)
+
+    return result.text
+
